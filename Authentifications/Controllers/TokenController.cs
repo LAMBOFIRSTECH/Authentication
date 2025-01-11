9b17d3b470d6b880d900e599127ca3e4ce4db69d
Artur-Lambo <lamboartur94@gmail.com>
2025-01-11 02:19:48 +0100

    Toujours pas capable de récupérer un email et le pass dans refreshToken dans le controller

--- Authentifications/Controllers/TokenController.cs
+++ Authentifications/Controllers/TokenController.cs
@@ -8,62 +8,62 @@ namespace Authentifications.Controllers;
 [Route("api/auth")]
 public class TokenController : ControllerBase
 {
-	private readonly JwtAccessAndRefreshTokenService jwtToken;
-	private readonly IRedisCacheTokenService redisTokenCache;
-	private readonly IRedisCacheService redisCache;
-	private readonly ILogger<JwtAccessAndRefreshTokenService> log;
-	private string? adress_mail;
-	private string? pass;
-	public TokenController(ILogger<JwtAccessAndRefreshTokenService> log, IRedisCacheTokenService redisTokenCache, JwtAccessAndRefreshTokenService jwtToken, IRedisCacheService redisCache)
-	{
-		this.jwtToken = jwtToken;
-		this.log = log;
-		this.redisTokenCache = redisTokenCache;
-		this.redisCache = redisCache;
-	}
-	/// <summary>
-	/// Authentifie un utilisateur et retourne les tokens (access et refresh).
-	/// </summary>
-	[HttpPost("login")]
-	public async Task<ActionResult> Authentificate()
-	{
-		var email = HttpContext.Items["email"] as string;
-		var password = HttpContext.Items["password"] as string;
-		var ticket = new AuthenticationTicket(User, "Basic");
-		if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
-			return BadRequest("Email or password is missing.");
-		if (!User.Identity!.IsAuthenticated)
-			return Unauthorized("Unauthorized access");
+    private readonly JwtAccessAndRefreshTokenService jwtToken;
+    private readonly IRedisCacheTokenService redisTokenCache;
+    private readonly IRedisCacheService redisCache;
+    private readonly ILogger<JwtAccessAndRefreshTokenService> log;
+    private string? email;
+    private string? password;
+    public TokenController(ILogger<JwtAccessAndRefreshTokenService> log, IRedisCacheTokenService redisTokenCache, JwtAccessAndRefreshTokenService jwtToken, IRedisCacheService redisCache)
+    {
+        this.jwtToken = jwtToken;
+        this.log = log;
+        this.redisTokenCache = redisTokenCache;
+        this.redisCache = redisCache;
+    }
+    /// <summary>
+    /// Authentifie un utilisateur et retourne les tokens (access et refresh).
+    /// </summary>
+    [HttpPost("login")]
+    public async Task<ActionResult> Authentificate()
+    {
+        email = HttpContext.Items["email"] as string;
+        password = HttpContext.Items["password"] as string;
+        var ticket = new AuthenticationTicket(User, "Basic");
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            return BadRequest("Email or password is missing.");
+        if (!User.Identity!.IsAuthenticated)
+            return Unauthorized("Unauthorized access");
 
-		var user = await jwtToken.AuthUserDetailsAsync((User.Identity!.IsAuthenticated, email, password));
-		var result = jwtToken.GetToken(user);
-		if (user.Pass == null)
-			return BadRequest("Password is missing.");
-		if (!result.Response)
-			return Unauthorized(new { result.Message });
+        var user = await jwtToken.AuthUserDetailsAsync((User.Identity!.IsAuthenticated, email, password));
+        var result = jwtToken.GetToken(user);
+        if (user.Pass == null)
+            return BadRequest("Password is missing.");
+        if (!result.Response)
+            return Unauthorized(new { result.Message });
 
-		adress_mail = email;
-		pass = password;
+        HttpContext.Items["email"] = email;
+        HttpContext.Items["password"] = password;
+        log.LogWarning("Stored email: {Email} and password: {Password} in HttpContext.Items", email, password);
 
-		log.LogWarning("Stored email: {Email} and password: {Password} in HttpContext.Items", email, password);
-
-		return CreatedAtAction(nameof(Authentificate), new { result.Token, result.RefreshToken });
-	}
-	/// <summary>
-	/// Rafraîchit le token en utilisant un refresh token valide.
-	///<param name="refreshToken"></param>
-	/// </summary>
-	[HttpPut("refreshToken")]
-	[AllowAnonymous]
-	public async Task<ActionResult> RegenerateAccessTokenUsingRefreshToken([FromBody] string refreshToken)
-	{
-		
-		log.LogWarning("Stored email: {Email} and password: {Password} in HttpContext.Items", adress_mail, adress_mail);
-		if (string.IsNullOrWhiteSpace(adress_mail) || string.IsNullOrWhiteSpace(pass))
-			return BadRequest("Email or password is missing.Could not refresh Token");
-		var result = await jwtToken.NewAccessTokenUsingRefreshTokenAsync(refreshToken, adress_mail!, pass!);
-		if (!result.Response)
-			return Unauthorized(new { result.Message });
-		return CreatedAtAction(nameof(RegenerateAccessTokenUsingRefreshToken), new { result.Token, result.RefreshToken });
-	}
+        return CreatedAtAction(nameof(Authentificate), new { result.Token, result.RefreshToken });
+    }
+    /// <summary>
+    /// Rafraîchit le token en utilisant un refresh token valide.
+    ///<param name="refreshToken"></param>
+    /// </summary>
+    [HttpPut("refreshToken")]
+    [AllowAnonymous]
+    public async Task<ActionResult> RegenerateAccessTokenUsingRefreshToken([FromBody] string refreshToken)
+    {
+        email = HttpContext.Items["email"] as string;
+        password = HttpContext.Items["password"] as string;
+        log.LogWarning("Stored email: {Email} and password: {Password} in HttpContext.Items", email, password);
+        if (string.IsNullOrWhiteSpace(email) || string.IsNullOrWhiteSpace(password))
+            return BadRequest("Email or password is missing.Could not refresh Token");
+        var result = await jwtToken.NewAccessTokenUsingRefreshTokenAsync(refreshToken, email!, password!);
+        if (!result.Response)
+            return Unauthorized(new { result.Message });
+        return CreatedAtAction(nameof(RegenerateAccessTokenUsingRefreshToken), new { result.Token, result.RefreshToken });
+    }
 }
